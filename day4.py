a332f5b3e63697e87bd5da64183c67c658d7f8f5
Sami <asqmoten@gmail.com>
2026-09-21 15:53:29 +0500

    Day 4 Complete

--- day4.py
+++ day4.py
@@ -1,18 +1,18 @@
-#machine_status = {
- #   "spinning_frame" : 12,
-  #  "weaving_loom" : 4,
-   # "carding_unit" : 0
-#}
+machine_status = {
+    "spinning_frame" : 12,
+    "weaving_loom" : 4,
+    "carding_unit" : 0
+}
 
-#for key,value in machine_status.items():
- #   if value==0:
-  #      print(f"ALERT {key} is completely offline!")
-   # elif value>0:
-    #    print(f"{key} : {value} units operating")
-#
-#machine_status["dyeing_vat"]=6
+for key,value in machine_status.items():
+    if value==0:
+        print(f"ALERT {key} is completely offline!")
+    elif value>0:
+        print(f"{key} : {value} units operating")
 
-#print(machine_status)
+machine_status["dyeing_vat"]=6
+
+print(machine_status)
 
 departments = [
     {"dept": "Spinning", "units": 1400, "rate_per_unit": 22},
